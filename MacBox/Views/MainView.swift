7acbb664c65f2c4a963d1415fcd0a3ae3add9495
zhihaofans <zhihaofans@hotmail.com>
2024-06-09 00:47:37 +0800

    移除用于学习测试使用的哔哩哔哩二维码页面代码，新增页面跳转功能

---
+++ MacBox/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  MacBox
+//
+//  Created by zzh on 2024/6/9.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ MacBox/Views/ToolsView.swift
@@ -0,0 +1,18 @@
+//
+//  ToolsView.swift
+//  MacBox
+//
+//  Created by zzh on 2024/6/8.
+//
+
+import SwiftUI
+
+struct ToolsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolsView()
+}
