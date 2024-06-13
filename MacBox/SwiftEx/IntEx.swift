dbec05ace154a070d426aab3f5fcff2f4a18ee7c
zhihaofans <zhihaofans@hotmail.com>
2024-06-13 22:44:57 +0800

    Add BoolEx, IntEx, StringEx

---
+++ MacBox/SwiftEx/IntEx.swift
@@ -0,0 +1,14 @@
+//
+//  IntEx.swift
+//  MacBox
+//
+//  Created by zzh on 2024/6/13.
+//
+
+import Foundation
+
+extension Int {
+    var toString: String {
+        return String(self)
+    }
+}
---
+++ MacBox/SwiftEx/StringEx.swift
@@ -0,0 +1,14 @@
+//
+//  StringEx.swift
+//  MacBox
+//
+//  Created by zzh on 2024/6/13.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
