a1be2e8637cd18eb9c5611429bfddfacc1feab2b
zhihaofans <zhihaofans@hotmail.com>
2024-07-23 23:59:41 +0800

    启动时自动生成二维码

--- MacBox/Views/QrcodeView.swift
+++ MacBox/Views/QrcodeView.swift
@@ -9,32 +9,32 @@ import SwiftUI
 import SwiftUtils
 
 struct QrcodeView: View {
-    @Binding var showPageId: String
     @State private var qrcodeStr = "Hello, Tools!"
     @State private var qrCodeImage: NSImage? = nil
     var body: some View {
-        TextField("", text: $qrcodeStr).onChange(of: qrcodeStr) { _, _ in
-            if qrcodeStr.isEmpty {
-                qrCodeImage = QrcodeUtil().generateQRCode(from: "Hello, Tools!")
-            }else{
-                qrCodeImage = QrcodeUtil().generateQRCode(from: qrcodeStr)
-            }
-            
-            
-        }
-        if qrCodeImage != nil {
-            Image(nsImage: qrCodeImage!)
-                .resizable()
-                .interpolation(.none)
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .contentShape(Rectangle()) // 加这行才实现可点击
-                .onTapGesture {
-                    // TODO: onClick
-                    
+        VStack {
+            TextField("", text: $qrcodeStr).onChange(of: qrcodeStr) { _, _ in
+                if qrcodeStr.isEmpty {
+                    qrCodeImage = QrcodeUtil().generateQRCode(from: "Hello, Tools!")
+                } else {
+                    qrCodeImage = QrcodeUtil().generateQRCode(from: qrcodeStr)
                 }
-        } else {
-            Text("生成中...")
+            }
+            if qrCodeImage != nil {
+                Image(nsImage: qrCodeImage!)
+                    .resizable()
+                    .interpolation(.none)
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .contentShape(Rectangle()) // 加这行才实现可点击
+                    .onTapGesture {
+                        // TODO: onClick
+                    }
+            } else {
+                Text("生成中...")
+            }
+        }.onAppear {
+            qrCodeImage = QrcodeUtil().generateQRCode(from: qrcodeStr)
         }
     }
 }
